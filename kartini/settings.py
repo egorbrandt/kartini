a5fbdea7f74c3b40cb679150c6cc8138225d86b5
egorbrandt <egorbrandt@gmail.com>
2023-02-27 13:11:19 +0100

    fifth

--- kartini/settings.py
+++ kartini/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'abc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = False
 
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['*']
 
