d5041ce96c4d4c49a758d80b586408bce141c349
egorbrandt <egorbrandt@gmail.com>
2023-02-27 11:56:29 +0100

    third key

--- kartini/settings.py
+++ kartini/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p*kb-$e+z+kn6bnrccm+yu6i+@(d8absf_%owc8_i7j*mvpdoz'
+SECRET_KEY = 'abc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
