69cda01c8c2ad2c95f0231274a22a15eef04227d
egorbrandt <egorbrandt@gmail.com>
2023-02-27 13:05:49 +0100

    fourth 404

--- kartini/settings.py
+++ kartini/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'abc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['*']
 
